721692df93e3dfbe57608a70a1b2d54fa13ade68
khakamora <yuriy.mamchurovskyy@gmail.com>
2015-10-07 22:59:41 +0300

    add new UI test

--- app/src/androidTest/java/home/com/f/quizactivity/MainActivityInstrumentationTest.java
+++ app/src/androidTest/java/home/com/f/quizactivity/MainActivityInstrumentationTest.java
@@ -19,8 +19,16 @@ import static android.support.test.espresso.action.ViewActions.click;
 import static android.support.test.espresso.action.ViewActions.closeSoftKeyboard;
 import static android.support.test.espresso.action.ViewActions.typeText;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.isCompletelyDisplayed;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static android.support.test.espresso.matcher.ViewMatchers.withText;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.RootMatchers.withDecorView;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.not;
 
 
 /**
@@ -31,10 +39,22 @@ import static android.support.test.espresso.matcher.ViewMatchers.withText;
 public class MainActivityInstrumentationTest{
 
     public static final String STRING_TO_BE_EXPECTED = "The Suez Canal connects the Red Sea and the Indian Ocean.";
+    public static final String INCORRECT_TOAST_TO_BE_EXPECTED = "Incorrect!";
 
     @Rule
     public ActivityTestRule<MainActivity> mActivityRule = new ActivityTestRule<>(
             MainActivity.class);
+
+    @Test
+    public void checkIncorrectAnswer() {
+        //Press the button.
+        onView(withId(R.id.true_button)).perform(click());
+
+        // Check that the text was appeared.
+        onView(withId(R.string.incorrect_toast)).check(matches(withText(INCORRECT_TOAST_TO_BE_EXPECTED)));
+        //onView(withId(R.string.incorrect_toast)).check(isDisplayed().matches(withText(INCORRECT_TOAST_TO_BE_EXPECTED)));
+   //     onView(withText(R.string.incorrect_toast)).inRoot(withDecorView(not(is(mActivityRule.getActivity().getWindow().getDecorView())))).check(matches(isDisplayed()));
+    }
 
     @Test
     public void checkNextQuestion() {
