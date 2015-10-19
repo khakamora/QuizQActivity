f010f6938ca526be5ceae51555be2d5b43f4e1b6
khakamora <yuriy.mamchurovskyy@gmail.com>
2015-10-19 21:24:03 +0300

    multiple activities test

--- app/src/androidTest/java/home/com/f/quizactivity/MultipleActivitiesTest.java
+++ app/src/androidTest/java/home/com/f/quizactivity/MultipleActivitiesTest.java
@@ -9,11 +9,16 @@ import android.app.Activity;
 import android.support.test.InstrumentationRegistry;
 import android.support.test.espresso.action.ViewActions;
 import android.support.test.espresso.matcher.ViewMatchers;
+import android.support.test.espresso.web.deps.guava.collect.Iterables;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
 import android.support.test.runner.lifecycle.ActivityLifecycleMonitorRegistry;
+import android.support.test.runner.lifecycle.Stage;
 import android.test.ActivityInstrumentationTestCase2;
 import android.test.suitebuilder.annotation.LargeTest;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import static android.support.test.espresso.Espresso.onView;
@@ -33,6 +38,7 @@ import static org.hamcrest.Matchers.is;
 import static org.hamcrest.Matchers.not;
 
 import static android.support.test.runner.lifecycle.Stage.RESUMED;
+
 import android.support.test.internal.runner.lifecycle.*;
 
 import java.util.Collection;
@@ -43,38 +49,50 @@ import java.util.Collection;
 
 @RunWith(AndroidJUnit4.class)
 @LargeTest
-public class MultipleActivitiesTest {
+public class MultipleActivitiesTest extends ActivityInstrumentationTestCase2 {
 
-/*
+    private  Activity currentActivity;
 
-     @Before
-    public void setUp() throws Exception {
-        super.setUp();
-        injectInstrumentation(InstrumentationRegistry.getInstrumentation());
-        mActivity = getActivity();
+    public MultipleActivitiesTest() {
+        super(MainActivity.class);
     }
 
-    public Activity getActivityInstance(){
+    public Activity getActivityInstance() {
         getInstrumentation().runOnMainSync(new Runnable() {
             public void run() {
-                Collection resumedActivities = ActivityLifecycleMonitorRegistry.getInstance().getActivitiesInStage(RESUMED);
-                if (resumedActivities.iterator().hasNext()){
-                    currentActivity = resumedActivities.iterator().next();
+                Collection<Activity> resumedActivities = ActivityLifecycleMonitorRegistry.getInstance().getActivitiesInStage(Stage.RESUMED);
+                for (Activity act : resumedActivities) {
+                    Log.d("Your current activity: ", act.getClass().getName());
+                    currentActivity = act;
+                    break;
                 }
             }
         });
 
         return currentActivity;
     }
-*/
 
+    Activity getCurrentActivity() throws Throwable {
+        getInstrumentation().waitForIdleSync();
+        final Activity[] activity = new Activity[1];
+        runTestOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                java.util.Collection<Activity> activites = ActivityLifecycleMonitorRegistry.getInstance().getActivitiesInStage(Stage.RESUMED);
+                activity[0] = Iterables.getOnlyElement(activites);
+            }
+        });
+        return activity[0];
+    }
 
 
     @Test
     public void checkIsCheater() {
 
+        Button b = (Button)currentActivity.findViewById(R.id.cheat_button);
+
         //Press the button.
-        onView(withId(R.id.cheat_button)).perform(click());
+        onView(withId(b)).perform(click());
 
 
     }
